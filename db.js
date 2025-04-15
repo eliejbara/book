15139b5bd34ff84831de5c8f15fe495afcf82586
Elie Jbara <ehj00@mail.aub.edu>
2025-04-15 14:19:04 +0300

    returned to local because the deployment did not work

--- db.js
+++ db.js
@@ -1,15 +1,18 @@
-// const mysql = require('mysql2');
+// const mysql = require("mysql2");
 
 // const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root', // or your MySQL username
-//   password: '', // add your MySQL password if you have one
-//   database: 'mangement'
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
 // });
 
 // db.connect((err) => {
-//   if (err) throw err;
-//   console.log('✅ Connected to MySQL (mangement)');
+//   if (err) {
+//     console.error("❌ DB connection error:", err);
+//     process.exit(1);
+//   }
+//   console.log("✅ Connected to Railway MySQL DB");
 // });
 
 // module.exports = db;
@@ -17,21 +20,20 @@
 
 
 
+
+
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: 'localhost',
+  user: 'root', // or your MySQL username
+  password: '', // add your MySQL password if you have one
+  database: 'mangement'
 });
 
 db.connect((err) => {
-  if (err) {
-    console.error('❌ DB connection error:', err);
-    process.exit(1);
-  }
-  console.log('✅ Connected to Railway MySQL DB');
+  if (err) throw err;
+  console.log('✅ Connected to MySQL (mangement)');
 });
 
 module.exports = db;
